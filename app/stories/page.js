ad0c42dd466d9ea5fcd72678013b515e18b34c3c
yougitgub <youssef842564@gmail.com>
2025-09-25 21:36:00 +0300

    .

--- app/stories/page.js
+++ app/stories/page.js
@@ -1,6 +1,4 @@
-import SolarSystem from '@/components/SolarSystem';
-import StoriesGrid from '../../components/StoriesGrid';
-import StoryCards from '@/components/StoryCards';
+
 import SpaceScene from '@/components/SpaceScene';
 export default function Page(){
   return(<>
